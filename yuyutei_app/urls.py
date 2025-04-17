c093f0ce9f22f06a08141c1482687c9b5519dcd9
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2025-04-17 17:22:51 +0800

    chore: remove polls

--- yuyutei_app/urls.py
+++ yuyutei_app/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include("polls.urls")),
     path('price_getter/', include("price_getter.urls")),
 ]
